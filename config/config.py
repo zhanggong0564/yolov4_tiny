a4e8d55283319cb315ce521342dd1b85620761cc
zhanggong <601806353@qq.com>
2021-11-12 15:22:20 +0800

    增加注释

--- config/config.py
+++ config/config.py
@@ -7,50 +7,48 @@ from utils.utils_bbox import DecodeBox
 
 cfg = EasyDict()
 
-cfg.train_dir = "./datas/2007_train.txt"
-cfg.val_dir = "./datas/2007_val.txt"
+cfg.train_dir = "./datas/2007_train.txt"                                                                #训练集info
+cfg.val_dir = "./datas/2007_val.txt"                                                                    #c集info
 # cfg.pretrained = 'datas/yolov4_tiny_weights_coco.pth'
-cfg.pretrained = None
+cfg.pretrained = None                                                                                   #与训练权重的；路径
 cfg.num_classes = 4
 
-cfg.use_multi_gpu = True
-cfg.input_shape = (416, 416)
-cfg.num_workers =4
-cfg.start_epoch = 0
-cfg.end_epoch = 100
-cfg.best_MAP = 0
-cfg.best_loss =10
+cfg.use_multi_gpu = True                                                                                #是否使用多GPU训练
+cfg.input_shape = (416, 416)                                                                            #网络输入的shape
+cfg.num_workers =0                                                                                      #多线程加载数据
+cfg.start_epoch = 0                                                                                     #从第几个expoch开始
+cfg.end_epoch = 100                                                                                     #总共训练多少个epoch
+cfg.best_MAP = 0                                                                                        #设置保存模型的最小MAP
+cfg.best_loss =10                                                                                       #设置保存模型的最小loss
 
 
-cfg.eval_epoch = 50
+cfg.eval_epoch = 5                                                                                      #设置验证map开始的epeoch
 
-cfg.num_anchors = 3
+cfg.num_anchors = 3                                                                                     #anchors 的数量
 cfg.best_epoch = 0
-cfg.decode = DecodeBox(np.array([[10., 14.],
-                                [23., 27.],
-                                [37., 58.],
-                                [81., 82.],
-                                [135., 169.],
-                                [344., 319.]]),
-                       3,
-                       (416,416),
-                       [[3, 4, 5], [1, 2, 3]])
+
 
 cfg.anchors = np.array([[10., 14.],
                     [23., 27.],
                     [37., 58.],
                     [81., 82.],
                     [135., 169.],
-                    [344., 319.]])
-cfg.anchors_mask = [[3, 4, 5], [1, 2, 3]]
-cfg.exp_log = "../logs/exp"
-cfg.weights_path = "../logs"
+                    [344., 319.]])                                                                       #anchors的宽高
+cfg.anchors_mask = [[3, 4, 5], [1, 2, 3]]                                                                #对应的anchors的mask
+
+cfg.decode = DecodeBox(cfg.anchors,
+                       cfg.num_classes,
+                       cfg.input_shape,
+                       cfg.anchors_mask)                                                                  #解码的类
+
+cfg.exp_log = "../logs/exp"                                                                               #记录日志的路径
+cfg.weights_path = "../logs"                                                                              #权重的路径
 
 cfg.train_ts = A.Compose([
         A.Resize(height=416,width=416),
         A.HorizontalFlip(p=0.5),
         A.RandomBrightnessContrast(p=0.2),
-    ], bbox_params=A.BboxParams(format='pascal_voc',label_fields=['class_labels']))
+    ], bbox_params=A.BboxParams(format='pascal_voc',label_fields=['class_labels']))                         #训练集的增强方式
 cfg.valid_ts = A.Compose([
         A.Resize(height=416,width=416),
-    ], bbox_params=A.BboxParams(format='pascal_voc',label_fields=['class_labels']))
+    ], bbox_params=A.BboxParams(format='pascal_voc',label_fields=['class_labels']))                         #验证集的增强方式
